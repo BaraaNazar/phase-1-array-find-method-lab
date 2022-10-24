d920280bd7b92cc57afcba3df3e03c2a792b2672
Baraa-Nazar <baraa.nazar.ph@gmail.com>
2022-10-25 01:11:48 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+]
+function superbowlWin( records ){
+    const result = records.find((r) => r.result === 'W')  
+    if (result ) return result.year
+  }
+  console.log(superbowlWin(record))
